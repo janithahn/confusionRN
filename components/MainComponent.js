9c95d6f5ef2383b89c5117a952760040cab5c2b0
s16417 <s16417@sci.pdn.ac.lk>
2020-07-12 09:28:43 +0530

    React Native Navigation Part 1

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,29 +1,33 @@
+import 'react-native-gesture-handler';
 import React, {Component} from 'react';
 import Menu from './MenuComponent';
 import {DISHES} from '../shared/dishes'
 import Dishdetail from './DishdetailComponent';
-import {View} from 'react-native';
+import {View, Platform} from 'react-native';
+import {createStackNavigator} from 'react-navigation';
+import Constants from 'expo-constants';
 
-class Main extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            dishes: DISHES,
-            selectedDish: null
+const MenuNavigator = createStackNavigator({
+    Menu: {screen: Menu}, 
+    Dishdetail: {screen: Dishdetail}
+}, {
+    initialRouteName: 'Menu',
+    navigationOptions: {
+        headerStyle: {
+            backgroundColor: '#512DA8'
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+            color: '#fff'
         }
     }
+});
 
-    onDishSelect(dishId) {
-        this.setState({selectedDish: dishId});
-    }
-
+class Main extends Component {
     render() {
         return(
-            <View style={{flex: 1}}>
-                <Menu dishes={this.state.dishes} 
-                    onPress={(dishId) => this.onDishSelect(dishId)}
-                />
-                <Dishdetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]}/>
+            <View style={{flex: 1, paddingTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight}}>
+                <MenuNavigator/>
             </View>
         );
     }
--- components/MenuComponent.js
+++ components/MenuComponent.js
@@ -1,29 +1,44 @@
 import React, {Component} from 'react';
 import {View, FlatList} from 'react-native';
 import {ListItem} from 'react-native-elements';
+import { DISHES } from '../shared/dishes';
 
-function Menu(props) {
+class Menu extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            dishes: DISHES
+        };
+    }
 
-    const renderMenuItem = ({item, index}) => {
+    static navigationOptions = {
+        title: 'Menu'
+    };
+
+    render(){
+        const renderMenuItem = ({item, index}) => {
+            return(
+                <ListItem
+                    key={index}
+                    title={item.name}
+                    subtitle={item.description}
+                    chevron={true}
+                    onPress={() => navigate('Dishdetail', {dishId: item.id})}
+                    leftAvatar={{source: {uri: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg'}}}
+                />
+            );
+        }
+
+        const {navigate} = this.props.navigation
+
         return(
-            <ListItem
-                key={index}
-                title={item.name}
-                subtitle={item.description}
-                chevron={true}
-                onPress={() => props.onPress(item.id)}
-                leftAvatar={{source: {uri: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg'}}}
+            <FlatList
+                data={this.state.dishes}
+                renderItem={renderMenuItem}
+                keyExtractor={item => item.id.toString()}
             />
         );
     }
-
-    return(
-        <FlatList
-            data={props.dishes}
-            renderItem={renderMenuItem}
-            keyExtractor={item => item.id.toString()}
-        />
-    );
 }
 
 export default Menu;
