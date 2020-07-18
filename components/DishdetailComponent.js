2df51362d973e1b8a1b064da1115ba19840db86a
s16417 <s16417@sci.pdn.ac.lk>
2020-07-18 17:08:02 +0530

    Social Sharing

--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {View, Text, ScrollView, FlatList, Modal, Button, StyleSheet, Alert, PanResponder} from 'react-native';
+import {View, Text, ScrollView, FlatList, Modal, Button, StyleSheet, Alert, PanResponder, Share} from 'react-native';
 import {Card, Icon, Rating, Input} from 'react-native-elements';
 import { baseUrl } from '../shared/baseurl';
 import { connect } from 'react-redux';
@@ -60,6 +60,16 @@ function RenderDish(props) {
         }
     });
 
+    const shareDish = (title, message, url) => {
+        Share.share({
+            title: title,
+            message: title + ': ' + message + ' ' + url,
+            url: url
+        },{
+            dialogTitle: 'Share ' + title
+        })
+    }
+
     if(dish != null) {
         return(
             <View>
@@ -87,6 +97,15 @@ function RenderDish(props) {
                                 type={'font-awesome'}
                                 color='#512DA8'
                                 onPress={() => props.handleCommentModal()}
+                            />
+                            <Icon
+                                raised
+                                reverse
+                                name='share'
+                                type='font-awesome'
+                                color='#51D2A8'
+                                style={styles.cardItem}
+                                onPress={() => shareDish(dish.name, dish.description, baseUrl + dish.image)}
                             />
                         </View>
                     </Card>
